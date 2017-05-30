27358216a4969ae20aecaeebc5f1bb7168919b66
Thanos Korakas <thanoskorakas@gmail.com>
2017-05-30 15:31:22 +0300

    Walls moved to seperate file

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 - Ghost respawn
 - Win criteria
 */
-
+;
 window.onload = function() {
   canvas = document.getElementById("pacman_canvas");
   canvasContext = canvas.getContext("2d");
@@ -72,83 +72,7 @@ let ghostsPosition = [
 ];
 
 // Initialize walls.
-const walls = [
-  // Outer walls.
-  [[0, 0], 570, 10],
-  [[560, 0], 10, 570],
-  [[0, 560], 570, 10],
-  [[0, 0], 10, 570],
-  [[0, 0], 10, 570],
-  // Ghost wall.
-  [[pillDistance * 6, pillDistance * 6], 10 + pillDistance, 10],
-  [[pillDistance * 8, pillDistance * 6], 10 + pillDistance, 10],
-  [[pillDistance * 6, pillDistance * 7], 10 + pillDistance * 3, 10],
-  // Other walls.
-  // Upper-left.
-  [[pillDistance, 0], 10, 10 + pillDistance * 4],
-  [[pillDistance, pillDistance * 5], 10, 10 + pillDistance * 3],
-  [[pillDistance * 2, pillDistance], 10 + pillDistance * 1, 10],
-  [[pillDistance * 4, pillDistance], 10 + pillDistance * 3, 10],
-  [[pillDistance, pillDistance * 2], 10 + pillDistance * 3, 10],
-  [[pillDistance * 5, pillDistance * 2], 10 + pillDistance * 3, 10],
-  [[pillDistance * 2, pillDistance * 3], 10 + pillDistance * 3, 10],
-  [[pillDistance * 6, pillDistance * 3], 10 + pillDistance * 1, 10],
-  [[pillDistance * 2, pillDistance * 4], 10, 10 + pillDistance * 2],
-  [[pillDistance * 2, pillDistance * 7], 10, 10 + pillDistance * 2],
-  [[pillDistance * 3, pillDistance * 4], 10, 10 + pillDistance * 3],
-  [[pillDistance * 8, 0], 10, 10 + pillDistance * 2],
-  [[pillDistance * 8, 0], 10 + pillDistance * 3, 10],
-  [[pillDistance * 8, pillDistance * 3], 10, 10 + pillDistance * 2],
-  [[pillDistance * 4, pillDistance * 4], 10 + pillDistance * 3, 10],
-  [[pillDistance * 5, pillDistance * 5], 10 + pillDistance * 3, 10],
-  [[pillDistance * 4, pillDistance * 5], 10, 10 + pillDistance * 2],
-  [[pillDistance * 4, pillDistance * 8], 10, 10 + pillDistance],
-  [[pillDistance * 3, pillDistance * 8], 10, 10 + pillDistance],
-  [[pillDistance * 3, pillDistance * 9], 10 + pillDistance * 3, 10],
-  [[pillDistance * 5, pillDistance * 6], 10, 10 + pillDistance * 2],
-  [[pillDistance * 6, pillDistance * 8], 10 + pillDistance * 2, 10],
-  // Upper-right.
-  [[pillDistance * 9, 0], 10, 10 + pillDistance * 2],
-  [[pillDistance * 9, pillDistance * 3], 10, 10 + pillDistance * 2],
-  [[pillDistance * 10, pillDistance], 10, 10 + pillDistance],
-  [[pillDistance * 10, pillDistance * 3], 10, 10 + pillDistance],
-  [[pillDistance * 10, pillDistance * 5], 10, 10 + pillDistance],
-  [[pillDistance * 11, pillDistance], 10 + pillDistance * 2, 10],
-  [[pillDistance * 11, pillDistance * 2], 10 + pillDistance * 2, 10],
-  [[pillDistance * 10, pillDistance * 3], 10 + pillDistance * 2, 10],
-  [[pillDistance * 13, pillDistance * 3], 10 + pillDistance, 10],
-  [[pillDistance * 10, pillDistance * 4], 10 + pillDistance, 10],
-  [[pillDistance * 12, pillDistance * 4], 10 + pillDistance, 10],
-  [[pillDistance * 12, pillDistance * 5], 10 + pillDistance, 10],
-  [[pillDistance * 11, pillDistance * 6], 10 + pillDistance * 2, 10],
-  [[pillDistance * 10, pillDistance * 5], 10 + pillDistance, 10],
-  [[pillDistance * 10, pillDistance * 7], 10 + pillDistance, 10],
-  [[pillDistance * 12, pillDistance * 7], 10 + pillDistance, 10],
-  [[pillDistance * 9, pillDistance * 8], 10 + pillDistance * 5, 10],
-  // Lower left.
-  [[pillDistance * 1, pillDistance * 9], 10, 10 + pillDistance * 3],
-  [[pillDistance * 1, pillDistance * 13], 10, 10 + pillDistance],
-  [[pillDistance * 1, pillDistance * 10], 10 + pillDistance * 3, 10],
-  [[pillDistance * 2, pillDistance * 11], 10 + pillDistance * 2, 10],
-  [[pillDistance * 2, pillDistance * 12], 10 + pillDistance * 2, 10],
-  [[pillDistance * 2, pillDistance * 13], 10 + pillDistance * 3, 10],
-  [[pillDistance * 5, pillDistance * 10], 10, 10 + pillDistance * 3],
-  [[pillDistance * 6, pillDistance * 10], 10, 10 + pillDistance * 2],
-  [[pillDistance * 7, pillDistance * 9], 10 + pillDistance * 2, 10],
-  [[pillDistance * 7, pillDistance * 10], 10 + pillDistance * 2, 10],
-  [[pillDistance * 7, pillDistance * 11], 10 + pillDistance * 2, 10],
-  [[pillDistance * 7, pillDistance * 12], 10 + pillDistance * 2, 10],
-  [[pillDistance * 6, pillDistance * 13], 10 + pillDistance * 2, 10],
-  // Lower right.
-  [[pillDistance * 10, pillDistance * 9], 10, 10 + pillDistance * 3],
-  [[pillDistance * 9, pillDistance * 13], 10 + pillDistance, 10],
-  [[pillDistance * 11, pillDistance * 9], 10, 10 + pillDistance * 2],
-  [[pillDistance * 11, pillDistance * 12], 10, 10 + pillDistance * 2],
-  [[pillDistance * 12, pillDistance * 9], 10 + pillDistance, 10],
-  [[pillDistance * 12, pillDistance * 10], 10 + pillDistance, 10],
-  [[pillDistance * 12, pillDistance * 11], 10 + pillDistance, 10 + pillDistance],
-  [[pillDistance * 12, pillDistance * 13], 10 + pillDistance, 10],
-];
+const walls = window.walls(pillDistance);
 
 // Initial value to create walls only once.
 let wallsCreated = false;
